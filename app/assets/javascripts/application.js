f54f8c4defd1918d9541286113ca5ba5a9594c9e
Gisle Thorsen <gisle.thorsen.91@gmail.com>
2018-06-14 17:31:06 +1000

    Fixed the issue where Jquery was undefined.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require_tree .
-
-//= require jquery
-//= require jquery_ujs
-//= require grayscale
 //= require bootstrap
