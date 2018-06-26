8929f71ee17f914a972dc778feae91e570023826
Gisle Thorsen <gisle.thorsen.91@gmail.com>
2018-06-26 22:14:28 +1000

    Added inline editing and markdown functionality to project description.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  #respond_to :html, :json
+
   def index
     @projects = Project.all
   end
@@ -9,5 +11,25 @@ class ProjectsController < ApplicationController
       redirect_back(fallback_location: projects_path)
       return
     end
+  end
+
+  def update
+    project = Project.find_by(url: params[:id])
+
+    respond_to do |format|
+      if project.update_attributes(project_params)
+        format.html { redirect_to(project, notice: 'Post was successfully updated.') }
+        format.json { respond_with_bip(project) }
+      else
+        format.html { redirect_to :back }
+        format.json { respond_with_bip(project) }
+      end
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:description)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def markdown(content)
+    renderer = CustomRedcarpetHTML.new(hard_wrap: true, filter_html: true, safe_links_only: true)
+    options = {
+      tables: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      space_after_headers: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
 end
